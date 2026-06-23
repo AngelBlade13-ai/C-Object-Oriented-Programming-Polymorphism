d520daf9aa40f85c1e05e07e8be9f9ce97ab51e5
AngelBlade13-ai <bladenmckinlay@gmail.com>
2026-06-23 12:45:23 -0600

    project completion

--- Program.cs
+++ Program.cs
@@ -1,1 +1,52 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace MyFirstProgram
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Polymorphism = Greek word tha means to "have many forms"
+            //               Objects can be identified by more than one type
+            //               Ex. A dog is also: Canine, Animal, Organism
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            Vehicle[] vehicles = {car, bicycle, boat};
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.Go();
+        }
+            Console.ReadKey();
+    }
+}
+    class Vehicle
+    {
+        public virtual void Go()
+        {
+          
+        }
+    }
+    class Car : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The car is moving");
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The bicycle is moving");
+        }
+    }
+    class Boat : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The boat is moving");
+        }
+}}
